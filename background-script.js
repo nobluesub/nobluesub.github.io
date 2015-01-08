b4843110d1b254dbc6ae3f54f830a309f2245648
nobluesub <nobluesub@gmail.com>
2015-01-08 12:01:11 +0100

    theme update + status update

--- background-script.js
+++ background-script.js
@@ -1,5 +1,5 @@
 function changeImg(imgNumber)	{
-var myImages = ["images/background4.jpg", "images/background5.jpg", "images/background6.jpg", "images/background7.jpg"];//["images/background.jpg", "images/background2.jpg", "images/background3.jpg"];
+var myImages = ["images/background4.jpg", "images/background6.jpg"];//["images/background.jpg", "images/background2.jpg", "images/background3.jpg"];
 var imgShown = document.body.style.backgroundImage;
 var newImgNumber =Math.floor(Math.random()*myImages.length);
 document.body.style.backgroundImage = 'url('+myImages[newImgNumber]+')';
