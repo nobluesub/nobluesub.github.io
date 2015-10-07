48e712726e8c6c9e6cbea8b35682cbdb8da567a3
nobluesub <nobluesub@gmail.com>
2015-10-08 01:23:01 +0200

    new dyn bg script

--- background-script.js
+++ background-script.js
@@ -1,7 +1,5 @@
-function changeImg(imgNumber)	{
-var myImages = ["images/background.jpg", "images/background9.jpg", "images/background2.jpg", "images/background7.jpg", "images/background8.jpg"];
-var imgShown = document.body.style.backgroundImage;
-var newImgNumber =Math.floor(Math.random()*myImages.length);
-document.body.style.backgroundImage = 'url('+myImages[newImgNumber]+')';
-}
-window.onload=changeImg;
+$(document).ready(function(){
+        var BackArray = ["images/background.jpg", "images/background9.jpg", "images/background2.jpg", "images/background7.jpg", "images/background8.jpg"];
+var fundo = BackArray[Math.floor(Math.random() * BackArray.length)];
+       $("body").css("background", '#ddd url('+ fundo +')');  
+        });
