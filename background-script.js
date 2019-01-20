1d24fa1a7d5570c48122270e9dcfc8bdf0cfafe8
nobluesub <Nobluesub>
2019-01-20 16:34:02 +0100

    remove winter backgrounds

--- background-script.js
+++ background-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-        var BackArray = ["images/background1.jpg", "images/background2.jpg", "images/background3.jpg", "images/background4.jpg", "images/background5.jpg", "images/background6.jpg", "images/background7.jpg"];
+        var BackArray = ["images/background1.jpg", "images/background2.jpg", "images/background3.jpg", "images/background4.jpg", "images/background5.jpg", "images/background6.jpg", "images/background7.jpg", "images/background8.jpg"];
 var fundo = BackArray[Math.floor(Math.random() * BackArray.length)];
        $("body").css("background", '#ddd url('+ fundo +')');  
         });
